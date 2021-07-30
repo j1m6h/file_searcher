614fcf2d60cfdaa489592f0a8ffcad04e2bde783
Joshua <44485177+joshuamatthewhagen@users.noreply.github.com>
2021-07-29 19:36:53 -0500

    Add files via upload

---
+++ file_finder.py
@@ -0,0 +1,27 @@
+import os
+
+print("FILE SEARCHER")
+print("Type 'help' for a list of commands... \n")
+
+dir_path = os.path.dirname(os.path.realpath("/"))
+results = 0
+
+while True:
+    buf = input("Enter a file name : ")
+
+    if buf == 'help':
+        print("exit - Terminate the program")
+        continue
+    if buf == 'exit':
+        break
+
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if str(file) == str(buf):
+                results += 1
+                print(f"Found file '{buf}' in {root}")
+
+    if results > 1 or 0:
+        print(f"Found ({results}) results.")
+    else:
+        print(f"Found ({results}) result.")
